fef36c99e80df4127cd636ef168215a17c4307e3
Davide Passerò <shardonian86@gmail.com>
2021-04-29 17:00:11 +0200

    form functionality completed

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const tabs = document.querySelectorAll('.slider__tab');
 const images = document.querySelectorAll('.slider__images');
 const content = document.querySelectorAll('.slider__content');
 
-// The reason why i've included the code in an if statement is to avoid
-// the error because DOM finish loading after JS code is executed.
+// The reason why i've included the event handlers in an if statement is to avoid
+// the error i had, because the DOM finish loading after JS code is executed.
+
 if(sliderContainer){
     // By giving a class to the button parent i can take advantage of 
-    // event delegation, capturing an bubbling instead of use a forEach
+    // event delegation, capturing and bubbling phases instead of use a forEach
     // for all buttons.
     sliderContainer.addEventListener('click', function(e) {
         const clicked = e.target;
@@ -19,7 +20,7 @@ if(sliderContainer){
     
         clicked.classList.add('underline__active');
         
-        // data-tab attribute makes change content possible by giving same numbers in classes.
+        // html data-tab attribute makes the content change possible by giving same numbers in classes.
         document.querySelector(`.slider__content--${clicked.dataset.tab}`).classList.add('slider__content--active');
         document.querySelector(`.slider__img--${clicked.dataset.tab}`).classList.add('slider__img--active');
     });
@@ -48,6 +49,7 @@ function setError(input, message) {
     small.innerText = message;
 };
 
+
 function checkInputs(inputsArray) {
     inputsArray.forEach(input => {
         if(input.value.trim() === '') {
@@ -56,17 +58,25 @@ function checkInputs(inputsArray) {
     });
 };
 
+function setUsernameLength() {
+    // Check also is not an empty string cause the functionality interfere with checkInputs()
+    // which means that when the field is empty it has to show a different error message
+    if(username.value.length < 3 || !username.value.trim() === '') {
+        setError(username, 'Name must contain at least 3 characters');
+    } else if(username.value.length > 30) {
+        setError(username, 'Name must contain max 30 characters');
+    }
+};
+
 function checkEmail(input) {
 	const emailRegexValidation = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if(!input.value.match(emailRegexValidation)) {
+    // Check also is not an empty string cause the functionality interfere with checkInputs()
+    // which means that when the field is empty it has to show a different error message
+    if(!input.value.match(emailRegexValidation) || !input.value.trim() === '') {
         setError(email, 'Please use a valid email address');
-    }  
+    } 
 };
-
-function resetForm() {
-    form.reset();
-}
 
 function checkSingolarPlural() {
     if(peopleNumber.textContent === '1') {
@@ -77,9 +87,14 @@ function checkSingolarPlural() {
 };
 
 function decreaseNumPeople(e) {
+    // Being part of a form, submit is the default event of the two buttons,
+    // in order to  prevent the behaviour the method needs to be here in this function
+    // because it is called in the eventListener below.
     e.preventDefault();
 
     if(peopleNumber.textContent > 1) {
+                                  // Pay attention: peopleNumber is not a number yet (in checkSingolarPlural
+                                  // function), plus sign (it's like parseInt) converts the string into a number
        peopleNumber.textContent = +peopleNumber.textContent -1;
        checkSingolarPlural();
     }
@@ -101,9 +116,9 @@ if(btnPlus) {btnPlus.addEventListener('click', increaseNumPeople)};
 if(form) {
     form.addEventListener('submit', function(e) {
         e.preventDefault();
-    
-        checkInputs([username, email, month, day, year, hour, minutes]);
+
+        setUsernameLength();
         checkEmail(email);
-        resetForm();
+        checkInputs([username, email, month, day, year, hour, minutes]);
      });
 };
